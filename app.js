659eb9c2f509dc19cf4008f085c9279d5b034375
Clair-Hawkes <hawkesj7@gmail.com>
2022-05-31 21:32:45 -0700

    Finish Functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+let counter = 0;
 
 // console.log("Let's get this party started!");
 
@@ -8,12 +9,28 @@
  * Creates an image element with the src attribute = the text input value.
  * returns the created image.
  */
- function createImg(){
+ async function createImg(){
   const $input = $('input').val();
+  const axiosArray = ["https://api.giphy.com/v1/gifs/search?q=",$input,"&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"];
+  const imgLink = await axios.get(axiosArray.join(''));
 
-  console.log('The Input = ',$input);
+  console.log(imgLink.data.data[0]);
+  const imgURL = imgLink.data.data[0].url;
+  const url = imgLink.data.data[counter].images.original.url;
+  // const fake = 'https://media0.giphy.com/media/iNZKRliHP4tI4mALtO/giphy.gif?cid=482277c26eteurkqwcrclzxt0716uxveqo13s0b5v71vwudz&rid=giphy.gif&ct=g'
+
+
+  // console.log(imgURL);
+  // console.log('The Input = ',$input);
   const $imgEle = $('<img>');
-  $imgEle.attr('src',$input);
+  $imgEle.attr('src',url);
+
+if(counter === 10){
+  counter = 0;
+} else {
+  counter++;
+}
+
   return $imgEle;
 }
 
@@ -22,15 +39,29 @@
  * Sets the img element src attribute = input form value.
  * returns undefined.
  */
-function appendImg(){
-  // evt.preventDefault();
+async function appendImg(evt){
+  console.log(evt);
+  evt.preventDefault();
 
   const $container = $('#container');
-  const imgEle = createImg();
-
+  const imgEle = await createImg();
+  console.log('imgEle = ',imgEle);
   $container.append(imgEle);
 }
 
+/**Function deleteMemes() takes no input
+ * Remvoes all children from the container.
+ */
+function deleteMemes(evt){
+  evt.preventDefault();
+  console.log(evt);
+
+  $('#container').empty();
+
+}
+
 //Event listener takes a function and that function needs the event.stopDefault()
+$('main').on('submit',appendImg);
+$('div').on('click',deleteMemes);
 
 
